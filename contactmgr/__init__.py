54035f034c389f06d2b90eb2ac89f7fe8b231ff8
Richard Neumann <mail@richard-neumann.de>
2021-03-25 11:14:31 +0100

    Add top-level exports

--- contactmgr/__init__.py
+++ contactmgr/__init__.py
@@ -1,1 +1,17 @@
 """Contacts manager."""
+
+from contactmgr.config import CONFIG
+from contactmgr.enumerations import ContactType
+from contactmgr.functions import get_contact, get_contacts
+from contactmgr.orm import Contact
+from contactmgr.wsgi import APPLICATION
+
+
+__all__ = [
+    'APPLICATION',
+    'CONFIG',
+    'Contact',
+    'ContactType',
+    'get_contact',
+    'get_contacts'
+]
