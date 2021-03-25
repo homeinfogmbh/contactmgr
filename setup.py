6fd9a3092d6c31b8e3e8fe5c36b9a4f430e20969
Richard Neumann <mail@richard-neumann.de>
2021-03-25 10:39:46 +0100

    Init.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+"""Install script."""
+
+from setuptools import setup
+
+
+setup(
+    name='contactmgr',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'flask',
+        'his',
+        'mdb',
+        'peewee',
+        'peeweeplus',
+        'wsgilib'
+    ],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info@homeinfo.de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r.neumann@homeinfo.de>',
+    packages=['contactmgr'],
+    license='GPLv3',
+    description='HIS microservice to handle contacts of customers.'
+)
